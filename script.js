7987bb767d7ea4df228ebb649749372246e3bfa5
Alan Timbó <alancorreiatimbo@gmail.com>
2022-08-15 17:53:02 -0300

    projeto3.8

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let prato;
 let bebida;
 let sobremesa;
 
+// função que habilita a seleção do prato
 function selecionarPrato(classeBotao) {
     const botaoSelecionado = document.querySelector('.pratos-principais .selecionado');
 
@@ -13,9 +14,17 @@ function selecionarPrato(classeBotao) {
 
     prato = classeBotao.innerHTML;
 
+    let plateSelected = document.querySelector('.pratos-principais .selecionado .prato-escolhido');
+    let pratoEscolhido = plateSelected.innerHTML;
+
+    let platePrice = document.querySelector('.pratos-principais .selecionado .valor');
+    let pratoPreco = platePrice.innerHTML;
+
     habilitarFechaPedido();
+    return pratoEscolhido, pratoPreco;
 }
 
+// função que habilita a seleção da bebida
 function selecionarBebida(Botao) {
     const botaoSelecionado = document.querySelector('.bebidas .selecionado');
 
@@ -27,9 +36,17 @@ function selecionarBebida(Botao) {
 
     bebida = Botao.innerHTML;
 
+    let drinkSelected = document.querySelector('.bebidas .selecionado .bebida-escolhida');
+    let bebidaEscolhida = drinkSelected.innerHTML;
+
+    let drinkPrice = document.querySelector('.bebidas .selecionado .valor');
+    let bebidaPreco = drinkPrice.innerHTML;
+
     habilitarFechaPedido();
+    return bebidaEscolhida, bebidaPreco;
 }
 
+// função que habilita a seleção da sobremesa
 function selecionarSobremesa(botao) {
     const botaoSelecionado = document.querySelector('.sobremesas .selecionado');
 
@@ -41,26 +58,59 @@ function selecionarSobremesa(botao) {
 
     sobremesa = botao.innerHTML;
 
+    let dessertSelected = document.querySelector('.sobremesas .selecionado .sobremesa-escolhida');
+    let sobremesaEscolhida = dessertSelected.innerHTML;
+
+    let dessertPrice = document.querySelector('.sobremesas .selecionado .valor');
+    let sobremesaPreco = dessertPrice.innerHTML;
+
     habilitarFechaPedido();
+    return sobremesaEscolhida, sobremesaPreco;
 }
 
+// função que habilita o botão "fechar pedido" após os 3 itens selecionados
 function habilitarFechaPedido() {
-    // SE o botão estiver verde, então NÃO continuar a função, então return
+    // SE o botão "fechar pedido" estiver escondido, ENTÃO o código vai prosseguir
+    const botaoFinal = document.querySelector(".fechar-pedido");
+    if (botaoFinal.classList.contains("escondido") === true) {
+        // SE o prato estiver selecionado
+        if (prato !== undefined) {
+            // SE a bebida estiver selecionada
+            if (bebida !== undefined) {
+                // SE a sobremesa estiver selecionada
+                if (sobremesa !== undefined) {
+                    // ENTÃO o botão "selecionar os 3 itens..." é escondido e o botão "fechar pedido" aparece
+                    const botaoFecharPedido = document.querySelector('.fechar-pedido.escondido');
 
-    if (prato !== undefined) {
-        if (bebida !== undefined) {
-            if (sobremesa !== undefined) {
-                const botaoFecharPedido = document.querySelector('.fechar-pedido.escondido');
-                console.log(botaoFecharPedido);
-                const botaoSelecionar3Itens = document.querySelector('.botao-pedido');
-                console.log(botaoSelecionar3Itens);
-                botaoFecharPedido.classList.remove('escondido');
-                botaoSelecionar3Itens.classList.add('escondido');
+                    const botaoSelecionar3Itens = document.querySelector('.botao-pedido');
 
+                    botaoFecharPedido.classList.remove('escondido');
+                    botaoSelecionar3Itens.classList.add('escondido');
+
+                }
             }
         }
     }
 }
+
+
+function somaPreco(pratoPreco, bebidaPreco, sobremesaPreco) {
+    let pratoPrice = Number(pratoPreco.replace(",", "."));
+    let bebidaPrice = Number(bebidaPreco.replace(",", "."));
+    let sobremesaPrice = Number(sobremesaPreco.replace(",", "."));
+    const price = pratoPrice + bebidaPrice + sobremesaPrice;
+
+    return price;
+}
+
+// função que envia a mensagem para o zapzap
+function enviarMensagem() {
+    let mensagem = `Olá, gostaria de fazer o pedido: \n- Prato: ${pratoEscolhido}\n- Bebida: ${bebidaEscolhida}\n- Sobremesa: ${sobremesaEscolhida}\nTotal: R$ ${somaPreco(pratoPreco, bebidaPreco, sobremesaPreco)}`;
+    let url = "https://wa.me/5521969425043?text=" + encondeURIComponent(mensagem);
+    window.open(url, _blanck);
+}
+
+
 
 // function fecharPedido() {
 //     // SE o prato principal estiver definido
